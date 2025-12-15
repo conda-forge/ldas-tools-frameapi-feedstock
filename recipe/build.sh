6a1fae17a69e200ca3ddc3ccbf2353248b93b9ec
Duncan Macleod <macleoddm@cardiff.ac.uk>
2025-12-15 11:37:16 +0000

    Convert from conda-build to rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,13 +8,13 @@ pushd _build
 
 # configure
 cmake \
-	${SRC_DIR} \
-	${CMAKE_ARGS} \
-	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
-	-DCMAKE_CROSSCOMPILING_EMULATOR:STRING="${CMAKE_CROSSCOMPILING_EMULATOR}" \
-	-DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=true \
-	-DCMAKE_OSX_ARCHITECTURES:STRING="${OSX_ARCH}" \
-	-DCMAKE_POLICY_VERSION_MINIMUM:STRING=3.5 \
+  ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
+  -DCMAKE_CROSSCOMPILING_EMULATOR:STRING="${CMAKE_CROSSCOMPILING_EMULATOR}" \
+  -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=true \
+  -DCMAKE_OSX_ARCHITECTURES:STRING="${OSX_ARCH}" \
+  -DCMAKE_POLICY_VERSION_MINIMUM:STRING=3.5 \
 ;
 
 # build
@@ -22,7 +22,7 @@ cmake --build . --parallel ${CPU_COUNT} --verbose
 
 # test (flaky on osx-64)
 if [[ "${target_platform}" == "linux-64" ]]; then
-	ctest --parallel ${CPU_COUNT} --verbose
+  ctest --parallel ${CPU_COUNT} --verbose
 fi
 
 # install
